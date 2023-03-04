44d0699545c8369e90db3903ec900e533690e120
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2023-03-04 20:41:27 +0300

    feat: add Trie class
    feat: add Contains method

---
+++ Homework2/Trie/Trie/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+
+var test = new Dictionary<char, int>();
+
+test.Add('c', 1);
+
+var ewkere = test['v'];
+
+Console.WriteLine(ewkere);
---
+++ Homework2/Trie/Trie/Trie.cs
@@ -0,0 +1,55 @@
+﻿namespace Trie;
+
+public class Trie
+{
+    private class Node
+    {
+        /// <summary>
+        /// Initialize class <see cref="Node"/>
+        /// </summary>
+        public Node()
+        {
+            next = new Dictionary<Char, Node>();
+        }
+
+        /// <summary>
+        /// Collection of next nodes
+        /// </summary>
+        public Dictionary<Char, Node> next;
+        /// <summary>
+        /// Is this element terminal for word
+        /// </summary>
+        public bool isTerminal;
+        /// <summary>
+        /// number of words that contain this element
+        /// </summary>
+        public int wordsCount;
+
+    }
+
+    private Trie()
+    {
+        head = new Node();
+    }
+
+    private Node head;
+
+    public int Size { get; set; }
+
+    public bool Contains(string element)
+    {
+        Node currentNode = head;
+
+        foreach(var symbol in element)
+        {
+            currentNode = currentNode.next[symbol];
+            if (currentNode == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
