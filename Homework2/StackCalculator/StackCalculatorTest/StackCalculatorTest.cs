e1fbda5e289373d4f4904c234c4fe53854d242e3
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2023-03-04 15:54:26 +0300

    test: add StackTest

---
+++ Homework2/StackCalculator/StackCalculatorTest/StackCalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿namespace StackCalculatorTest;
+
+using StackCalculator;
+
+public class StackCalculatorTest
+{
+    private readonly float delta = 0.00001F;
+
+    private static IEnumerable<TestCaseData> StackCalculator()
+    {
+        yield return new TestCaseData(new PostfixCalculator(new StackArray()));
+        yield return new TestCaseData(new PostfixCalculator(new StackList()));
+    }
+
+    [TestCaseSource(nameof(StackCalculator))]
+    public void CorrectPostfixExpressionTest(PostfixCalculator postfixCalculator)
+    {
+        var expression = "1 2 + 3 4 - +";
+        var expectedValue = 2F;
+
+        (var actualValue, var actualIsExecuted) = postfixCalculator.CalculatePostfixExpression(expression);
+
+        Assert.That(actualIsExecuted && Math.Abs(expectedValue - actualValue) < delta);
+    }
+}
---
+++ Homework2/StackCalculator/StackCalculatorTest/StackTest.cs
@@ -0,0 +1,54 @@
+﻿namespace StackCalculatorTest;
+
+using StackCalculator;
+
+public class Tests
+{
+    private static IEnumerable<TestCaseData> Stack()
+    {
+        yield return new TestCaseData(new StackArray());
+        yield return new TestCaseData(new StackList());
+    }
+
+    [TestCaseSource(nameof(Stack))]
+    public void StackRandomPushAndPopTest(IStack stack)
+    {
+        var random = new Random();
+        var inputStackArray = new float[random.Next(50)];
+        for (int i = 0; i < inputStackArray.Length; ++i)
+        {
+            inputStackArray[i] = random.Next();
+            stack.Push(inputStackArray[i]);
+        }
+
+        var popStackArray = new float[inputStackArray.Length];
+        for (int i = 0; i < inputStackArray.Length; ++i)
+        {
+            popStackArray[inputStackArray.Length - i - 1] = stack.Pop();
+        }
+
+        Assert.That(inputStackArray, Is.EqualTo(popStackArray));
+    }
+
+    [TestCaseSource(nameof(Stack))]
+    public void StackIsEmptyTest(IStack stack)
+    {
+        var isNewStackEmpty = stack.IsEmpty();
+
+        stack.Push(0.0F);
+
+        var isStackWithOneElementEmpty = stack.IsEmpty();
+
+        stack.Pop();
+
+        var isStackWithLastDeletedElementEmpty = stack.IsEmpty();
+
+        Assert.That(isNewStackEmpty && isStackWithLastDeletedElementEmpty && !isStackWithOneElementEmpty);
+    }
+
+    [TestCaseSource(nameof(Stack))]
+    public void PopEmptyStackTest(IStack stack)
+    {
+        Assert.Throws<InvalidOperationException>(() => stack.Pop());
+    }
+}
