601375427591f3a80e4ba5f0dbbf9c1a9f670bec
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2023-03-04 22:49:16 +0300

    feat: add Add and Remove methods

--- Homework2/Trie/Trie/Program.cs
+++ Homework2/Trie/Trie/Program.cs
@@ -1,9 +1,3 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Trie;
 
-var test = new Dictionary<char, int>();
-
-test.Add('c', 1);
-
-var ewkere = test['v'];
-
-Console.WriteLine(ewkere);
+var test = new ();
--- Homework2/Trie/Trie/Trie.cs
+++ Homework2/Trie/Trie/Trie.cs
@@ -5,7 +5,7 @@ public class Trie
     private class Node
     {
         /// <summary>
-        /// Initialize class <see cref="Node"/>
+        /// Initialize class <see cref="Node"/>.
         /// </summary>
         public Node()
         {
@@ -13,15 +13,15 @@ public class Trie
         }
 
         /// <summary>
-        /// Collection of next nodes
+        /// Collection of next nodes.
         /// </summary>
         public Dictionary<Char, Node> next;
         /// <summary>
-        /// Is this element terminal for word
+        /// Is this element terminal for word.
         /// </summary>
         public bool isTerminal;
         /// <summary>
-        /// number of words that contain this element
+        /// number of words that contain this element.
         /// </summary>
         public int wordsCount;
 
@@ -34,22 +34,119 @@ public class Trie
 
     private Node head;
 
-    public int Size { get; set; }
+    private int _size = 1;
+    public int Size {
+        get
+        {
+            return _size;
+        }
+        private set
+        {
+            _size = value;
+        }
+    }
 
+    /// <summary>
+    /// Method to check is element contained in Trie.
+    /// </summary>
+    /// <param name="element">Еhe string to be checked to see if it is contained in.</param>
+    /// <returns>true -- if Trie contains element, false -- if it doesn't.</returns>
+    /// <exception cref="ArgumentNullException">element variable can't be null.</exception>
     public bool Contains(string element)
     {
+        if (element == null)
+        {
+            throw new ArgumentNullException(nameof(element), "Can't be null");
+        }
+
         Node currentNode = head;
 
         foreach(var symbol in element)
         {
-            currentNode = currentNode.next[symbol];
-            if (currentNode == null)
+            if (!currentNode.next.ContainsKey(symbol))
             {
                 return false;
             }
+
+            currentNode = currentNode.next[symbol];
         }
+
+        return currentNode.isTerminal;
+    }
+
+    /// <summary>
+    /// Method for adding an element to a Trie.
+    /// </summary>
+    /// <param name="element">element, that need to be added to Trie</param>
+    /// <returns>true -- it successfully added the element, false -- the element is already contained (Empty string contains in the Trie)</returns>
+    /// <exception cref="ArgumentNullException">element can't be null</exception>
+    public bool Add(string element)
+    {
+        if (element == null)
+        {
+            throw new ArgumentNullException(nameof(element), "Can't be null");
+        }
+
+        if (Contains(element))
+        {
+            return false;
+        }
+
+        var currentNode = head;
+
+        foreach(var symbol in element)
+        {
+            ++currentNode.wordsCount;
+
+            if (!currentNode.next.ContainsKey(symbol))
+            {
+                currentNode.next.Add(symbol, new Node());
+                ++_size;
+            }
+
+            currentNode = currentNode.next[symbol];
+        }
+
+        ++currentNode.wordsCount;
+
+        return currentNode.isTerminal = true;
+    }
+
+    public bool Remove(string element)
+    {
+        if (element == null)
+        {
+            throw new ArgumentNullException(nameof(element), "Can't be null");
+        }
+
+        if (element == "")
+        {
+            throw new ArgumentException("Can't to remove empty string", nameof(element));
+        }
+
+        if (!Contains(element))
+        {
+            return false;
+        }
+
+        var currentNode = head;
+
+        for (int i = 0; i < element.Length; ++i)
+        {
+            --currentNode.wordsCount;
+
+            if (currentNode.next[element[i]].wordsCount == 1)
+            {
+                currentNode.next.Remove(element[i]);
+                return true;
+            }
+
+            currentNode = currentNode.next[element[i]];
+        }
+
+        --currentNode.wordsCount;
+        currentNode.isTerminal = false;
 
         return true;
     }
 }
-
