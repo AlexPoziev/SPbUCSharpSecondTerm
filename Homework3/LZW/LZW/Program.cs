14a2b260e36feee7fc9b814cdf853151e7b6e90e
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2023-03-11 15:21:28 +0300

    feat: add BWT library, tried change of extension
    Probably it'll need fixes

--- Homework3/LZW/LZW/Program.cs
+++ Homework3/LZW/LZW/Program.cs
@@ -1,15 +1,29 @@
 ﻿using Trees;
+using Algorithms;
+using System.Linq;
 
-var trie = new Trie();
+//var trie = new Trie();
 
-trie.Add("e", 0);
-trie.Add("eb", 1);
-trie.Add("ebc", 4);
-trie.Add("ebce", 5);
+//trie.Add("e", 0);
+//trie.Add("eb", 1);
+//trie.Add("ebc", 4);
+//trie.Add("ebce", 5);
 
-var test = trie.GetAllValuesOfElement("ebc") ?? new int[] { 1, 2 };
+//var test = trie.GetAllValuesOfElement("ebc") ?? new int[] { 1, 2 };
 
-foreach (var element in test)
-{
-    Console.WriteLine(element);
-}
+//foreach (var element in test)
+//{
+//    Console.WriteLine(element);
+//}
+
+var filePath = args[0];
+var useKey = args[1];
+
+var lastElementIndex = 0;
+var newFilePath = filePath.Substring(0, filePath.LastIndexOf('.')) + ".zipped";
+string fileContent = File.ReadAllText(filePath);
+File.Create(newFilePath).Close();
+
+(fileContent, var result) = BWT.DirectBWT(fileContent);
+
+File.WriteAllText(newFilePath, fileContent + $"${result}");
