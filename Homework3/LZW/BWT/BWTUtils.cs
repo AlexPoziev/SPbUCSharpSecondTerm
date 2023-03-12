8d02e8f210954b63b356affe57c5af4241722548
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2023-03-12 23:14:17 +0300

    refactor: BWT to byteArray

--- Homework3/LZW/BWT/BWTUtils.cs
+++ Homework3/LZW/BWT/BWTUtils.cs
@@ -12,34 +12,34 @@ public static class BWTSort
     // function to compare two suffix of <word>, takes two suffix indexes
     // returns true if the first suffix more than the second
     // throw exception if word == null
-    private static bool IsSuffixMore(in string word, int firstSuffixIndex, int secondSuffixIndex)
+    private static bool IsSuffixMore(in byte[] bytes, int firstSuffixIndex, int secondSuffixIndex)
     {
-        if (word == null)
+        if (bytes == null)
         {
-            throw new ArgumentNullException(word, "Can't be null");
+            throw new ArgumentNullException(nameof(bytes), "Can't be null");
         }
 
-        if (word == "")
+        if (!bytes.Any())
         {
-            throw new ArgumentException("Empty string doesn't have suffixes", word);
+            throw new ArgumentException("Empty string doesn't have suffixes", nameof(bytes));
         }
 
-        if (firstSuffixIndex < 0 || firstSuffixIndex > word.Length - 1)
+        if (firstSuffixIndex < 0 || firstSuffixIndex > bytes.Length - 1)
         {
             throw new ArgumentOutOfRangeException(nameof(firstSuffixIndex), "Last element index can't be less than zero and more than string length - 1");
         }
 
-        if (secondSuffixIndex < 0 || secondSuffixIndex > word.Length - 1)
+        if (secondSuffixIndex < 0 || secondSuffixIndex > bytes.Length - 1)
         {
             throw new ArgumentOutOfRangeException(nameof(secondSuffixIndex), "Last element index can't be less than zero and more than string length - 1");
         }
 
 
-        for (int i = 0; i < word.Length; ++i)
+        for (int i = 0; i < bytes.Length; ++i)
         {
-            if (word[(firstSuffixIndex + i) % word.Length] != word[(secondSuffixIndex + i) % word.Length])
+            if (bytes[(firstSuffixIndex + i) % bytes.Length] != bytes[(secondSuffixIndex + i) % bytes.Length])
             {
-                return word[(firstSuffixIndex + i) % word.Length] > word[(secondSuffixIndex + i) % word.Length];
+                return bytes[(firstSuffixIndex + i) % bytes.Length] > bytes[(secondSuffixIndex + i) % bytes.Length];
             }
         }
 
@@ -51,11 +51,11 @@ public static class BWTSort
     // word immutable
     // throw exception if wrod == null
     // int array must contains array with numbers in range 0..word.Length - 1. They must not to stay in a row
-    public static int DirectBWTSort(in string word, int[] array)
+    public static int DirectBWTSort(in byte[] bytes, int[] array)
     {
-        if (word == null)
+        if (bytes == null)
         {
-            throw new ArgumentNullException(word, "Can't be null");
+            throw new ArgumentNullException(nameof(bytes), "Can't be null");
         }
 
         if (array == null)
@@ -63,12 +63,12 @@ public static class BWTSort
             throw new ArgumentNullException(nameof(array), "Can't be null");
         }
 
-        if (word == "")
+        if (!bytes.Any())
         {
-            throw new ArgumentException("Can't to sort empty string", word);
+            throw new ArgumentException("Can't to sort empty string", nameof(bytes));
         }
 
-        if (!IsArrayFilledBySequenceRight(word.Length, array))
+        if (!IsArrayFilledBySequenceRight(bytes.Length, array))
         {
             throw new ArgumentException("Array filled not right(not range 0..word.Length - 1)", nameof(array));
         }
@@ -87,7 +87,7 @@ public static class BWTSort
         {
             for (var i = gap; i < array.Length; ++i)
             {
-                for (var j = i; j - gap >= 0 && IsSuffixMore(word, array[j - gap], array[j]); j -= gap)
+                for (var j = i; j - gap >= 0 && IsSuffixMore(bytes, array[j - gap], array[j]); j -= gap)
                 {
                     if (j == lastElement || j - gap == lastElement)
                     {
@@ -109,11 +109,11 @@ public static class BWTSort
     // sort of ints by word values. Mutate ints. Sorting must be stable
     // throw exception if word == null
     // ints array must contains array with numbers in range 0..word.Length - 1. They must not to stay in a row
-    public static void InverseBWTSort(in string word, int[] ints)
+    public static void InverseBWTSort(in byte[] bytes, int[] ints)
     {
-        if (word == null)
+        if (bytes == null)
         {
-            throw new ArgumentNullException(word, "Can't be null");
+            throw new ArgumentNullException(nameof(bytes), "Can't be null");
         }
 
         if (ints == null)
@@ -121,21 +121,21 @@ public static class BWTSort
             throw new ArgumentNullException(nameof(ints), "Can't be null");
         }
 
-        if (word == "")
+        if (!bytes.Any())
         {
-            throw new ArgumentException("Can't sort empty string", word);
+            throw new ArgumentException("Can't sort empty string", nameof(bytes));
         }
 
-        if (!IsArrayFilledBySequenceRight(word.Length, ints))
+        if (!IsArrayFilledBySequenceRight(bytes.Length, ints))
         {
             throw new ArgumentException("Array filled not right(not range 0..word.Length - 1)", nameof(ints));
         }
 
-        for (int i = 0; i < word.Length - 1; ++i)
+        for (int i = 0; i < bytes.Length - 1; ++i)
         {
-            for (int j = 0; j < word.Length - 1 - i; ++j)
+            for (int j = 0; j < bytes.Length - 1 - i; ++j)
             {
-                if (word[ints[j]] > word[ints[j + 1]])
+                if (bytes[ints[j]] > bytes[ints[j + 1]])
                 {
                     Swap(ref ints[j], ref ints[j + 1]);
                 }
