3e97ec4b0e0dd35727b811a5126576cb10ebb7ce
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2023-04-08 08:06:16 +0300

    feat: add UI

---
+++ Homework7/Calculator/Calculator/Calculator.cs
@@ -0,0 +1,55 @@
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void OperationsTable_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void NumberClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OperationClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResultClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeleteLastSymbolClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClearClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
