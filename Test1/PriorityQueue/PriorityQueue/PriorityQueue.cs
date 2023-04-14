bcf07cc261fd4747c06f4faf0ff19bd9ed8667a2
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2023-04-14 13:59:49 +0300

    feat: add Enqueue method

---
+++ Test1/PriorityQueue/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,82 @@
+﻿/* Copyright 2023-2024 Aleksey Poziev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
+namespace PriorityQueue;
+
+public class PriorityQueue<T>
+{
+    private PriorityQueueElement? head;
+
+    public bool Empty => head == null;
+
+    public void Enqueue(T value, int priority)
+    {
+        var newElement = new PriorityQueueElement(value, priority);
+
+        if (head == null)
+        {
+            head = newElement;
+
+            return;
+        }
+
+        var elementToAddAfterIt = LastElementWithPriorityMoreThanOrEqualsGiven(priority);
+
+        if (elementToAddAfterIt == null)
+        {
+            newElement.NextElement = head.NextElement;
+            head = newElement;
+        }
+        else
+        {
+            newElement.NextElement = elementToAddAfterIt.NextElement;
+            elementToAddAfterIt.NextElement = newElement;
+        }
+    }
+
+    private PriorityQueueElement? LastElementWithPriorityMoreThanOrEqualsGiven(int priority)
+    {
+        if (head == null)
+        {
+            return null;
+        }
+
+        var currentElement = head;
+        PriorityQueueElement? previousElement = null;
+
+        while (currentElement != null && currentElement.Priority >= priority)
+        {
+            previousElement = currentElement;
+            currentElement = currentElement.NextElement;
+        }
+
+        return previousElement;
+    }
+
+    private class PriorityQueueElement
+    {
+        public PriorityQueueElement(T value, int priority)
+        {
+            Value = value;
+            Priority = priority;
+        }
+
+        public T Value { get; }
+
+        public int Priority { get; }
+
+        public PriorityQueueElement? NextElement { get; set; }
+    }
+}
+
