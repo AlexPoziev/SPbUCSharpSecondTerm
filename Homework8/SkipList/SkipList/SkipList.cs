ff893302c010fe812f7fe3bbb1de024fc03e3825
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2023-04-26 11:47:58 +0300

    feat: add Insert and Contains Methods

--- Homework8/SkipList/SkipList/SkipList.cs
+++ Homework8/SkipList/SkipList/SkipList.cs
@@ -2,11 +2,11 @@
 
 public class SkipList<T> : IList<T> where T : IComparable
 {
-    private readonly SkipListNode head = new (default, new LinkedList<SkipListNode?>());
+    private SkipListNode head = new (default, new SkipListNode[1]);
 
-    private readonly SkipListNode tail = new (default, new LinkedList<SkipListNode?>());
+    private readonly SkipListNode tail = new (default, Array.Empty<SkipListNode>());
 
-    private LinkedList<SkipListNode> GetPreviousOfSearchedByValue(T value)
+    private SkipListNode[] GetPreviousOfSearchedByValue(T value)
     {
         if (value == null)
         {
@@ -14,18 +14,18 @@ public class SkipList<T> : IList<T> where T : IComparable
         }
 
         var currentNode = head;
-        SkipListNode? previousNode = null;
-        var lastLevelPreviousNodesList = new LinkedList<SkipListNode>();
 
-        for (var currentLevel = head.Next.Count; currentLevel > 0; --currentLevel)
+        var lastLevelPreviousNodesList = new SkipListNode[head.Next.Length];
+
+        for (var currentLevel = head.Next.Length; currentLevel > 0; --currentLevel)
         {
-            var compareResult = currentNode.Next.ElementAt(currentLevel).Value.CompareTo(value);
-            while (compareResult == 1 && currentNode.Next.ElementAt(currentLevel) != tail)
+            var compareResult = currentNode.Next[currentLevel].Value.CompareTo(value);
+            while (compareResult == 1 && currentNode.Next[currentLevel] != tail)
             {
-                currentNode = currentNode.Next.ElementAt(currentLevel);
+                currentNode = currentNode.Next[currentLevel];
             }
 
-            lastLevelPreviousNodesList.AddFirst(currentNode);
+            lastLevelPreviousNodesList[currentLevel] = currentNode;
         }
 
         return lastLevelPreviousNodesList;
@@ -35,21 +35,75 @@ public class SkipList<T> : IList<T> where T : IComparable
     {
         var resultOfSearch = GetPreviousOfSearchedByValue(value);
 
-        return resultOfSearch.ElementAt(0) != tail && resultOfSearch.ElementAt(0).Value.CompareTo(value) == 0;
+        return resultOfSearch[0] != tail && resultOfSearch[0].Value.CompareTo(value) == 0;
+    }
+
+    public void Insert(int index, T value)
+    {
+
+    }
+
+    public void Add(T value)
+    {
+        var previousNodes = GetPreviousOfSearchedByValue(value);
+
+        CreateAndPutRandomHeightSkipListNode(value, previousNodes);
+    }
+
+    public void Clear()
+    {
+        head = new(default, new SkipListNode[1]);
+        head.Next[0] = tail;
+    }
+
+    private SkipListNode CreateAndPutRandomHeightSkipListNode(T value, SkipListNode[] previousNodes)
+    {
+        var newNodeSize = GetRandomSkipListNodeHeight();
+
+        var newSkipListNode = new SkipListNode(value, new SkipListNode[newNodeSize]);
+
+        for (int i = 0; i < newNodeSize && i < head.Next.Length; ++i)
+        {
+            newSkipListNode.Next[i] = previousNodes[i].Next[i];
+            previousNodes[i].Next[i] = newSkipListNode;
+        }
+
+        if (newNodeSize > head.Next.Length)
+        {
+            var newHeadNext = new SkipListNode(default, new SkipListNode[newNodeSize]);
+            head.Next.CopyTo(newHeadNext.Next, 0);
+            newHeadNext.Next[newNodeSize - 1] = newSkipListNode;
+            head = newHeadNext;
+        }
+
+        return newSkipListNode;
+    }
+
+    private int GetRandomSkipListNodeHeight()
+    {
+        const int increaseNumber = 1;
+
+        var random = new Random();
+
+        var currentSize = 1;
+
+        while (currentSize <= head.Next.Length)
+        {
+            if (random.Next(0, 2) != increaseNumber)
+            {
+                break;
+            }
+
+            ++currentSize;
+        }
+
+        return currentSize;
     }
 
     bool ICollection<T>.IsReadOnly => false;
 
     public int Count { get; }
 
-    private record SkipListNode(T? Value, LinkedList<SkipListNode?> Next);
-
-    List
-
-    public SkipList()
-    {
-        head.Next.AddFirst(tail);
-    }
-
+    private record SkipListNode(T? Value, SkipListNode[] Next);
 }
 
