8a8fa9d0769554834e65cfc0bbf4078f38e63ded
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2023-08-14 16:12:49 +0300

    feat: add secondTask

---
+++ TestWork/Palindrome/Palindrome/Palindrome/Palindrome.cs
@@ -0,0 +1,68 @@
+﻿/* Copyright 2023-2024 Aleksey Poziev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
+namespace FirstTask;
+
+/// <summary>
+/// class for working with palindromes.
+/// </summary>
+public static class Palindrome
+{
+    /// <summary>
+    /// method to check is string is a palindrome.
+    /// </summary>
+    /// <param name="expression">string that need to be checked.</param>
+    /// <returns>true -- string is palindrome, false -- is not palindrome.</returns>
+    /// <exception cref="ArgumentNullException">Can't to check is null on palindrome.</exception>
+    public static bool IsPalindrome(string expression)
+    {
+        ArgumentNullException.ThrowIfNull(expression);
+
+        expression = expression.ToLower();
+        int leftSide = 0;
+        int rightSide = expression.Length - 1;
+
+        if (rightSide == -1)
+        {
+            return true;
+        }
+
+        while (leftSide < rightSide)
+        {
+            while (leftSide < rightSide && expression[leftSide] == ' ')
+            {
+                ++leftSide;
+            }
+            while (leftSide < rightSide && expression[rightSide] == ' ')
+            {
+                --rightSide;
+            }
+
+            if (leftSide >= rightSide)
+            {
+                return true;
+            }
+
+            if (expression[leftSide] != expression[rightSide])
+            {
+                return false;
+            }
+
+            ++leftSide;
+            --rightSide;
+        }
+
+        return true;
+    }
+}
---
+++ TestWork/Palindrome/Palindrome/PalindromeTest/PalindromeTest.cs
@@ -0,0 +1,15 @@
+﻿using FirstTask;
+
+namespace PalindromeTest;
+
+public class Tests
+{
+    [TestCase("", true)]
+    [TestCase("test", false)]
+    [TestCase("lol  ", true)]
+    [TestCase("Я иду с мечем судия", true)]
+    public void IsPalindromeShouldReturnExpectedResult(string expression, bool expectedResult)
+    {
+        Assert.That(Palindrome.IsPalindrome(expression), Is.EqualTo(expectedResult));
+    }
+}
